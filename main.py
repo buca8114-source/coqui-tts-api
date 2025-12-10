27040c6a2ccccf2dc0b3a2aba853b43e98f27fcd
buca8114-source <buca8114@gmail.com>
2025-12-10 14:44:54 +0300

    main.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pathlib import Path
-import subprocess
 import uuid
-import uvicorn
+from TTS.api import TTS
 
 app = FastAPI()
+
+# Model sadece ilk başlatmada indirilir ve RAM'e alınır
+tts = TTS("tts_models/tr/common-finetuned_ljspeech").to("cpu")
 
 @app.get("/")
 def home():
     return {"status": "Coqui TTS API Active 🎙️"}
 
 @app.get("/tts")
-def tts(text: str):
+def generate(text: str):
     file_name = f"audio_{uuid.uuid4()}.wav"
     output_path = Path(file_name)
 
-    command = f'tts --text "{text}" --out_path "{output_path}"'
-    subprocess.call(command, shell=True)
+    tts.tts_to_file(text=text, file_path=output_path)
 
     return FileResponse(output_path, media_type="audio/wav", filename=file_name)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=10000)
