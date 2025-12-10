1327c5a5b2c4fafb5d426a08d3148600e12c6970
buca8114-source <buca8114@gmail.com>
2025-12-10 14:14:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pathlib import Path
-import uuid
 import subprocess
+import uuid
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "Coqui TTS API Active 🚀"}
+    return {"status": "Coqui TTS API Active 🎙️"}
 
 @app.get("/tts")
 def tts(text: str):
@@ -19,3 +20,7 @@ def tts(text: str):
     subprocess.call(command, shell=True)
 
     return FileResponse(output_path, media_type="audio/wav", filename=file_name)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=10000)
